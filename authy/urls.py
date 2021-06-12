f3ce6ebef6ed11d79e3a8c648c7889907d4d32e8
Haris Awan <haris.akram764@gmial.com>
2021-06-12 22:55:44 +0500

    make it better

--- authy/urls.py
+++ authy/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
    	
     path('profile/edit', EditProfile, name='edit-profile'),
    	path('signup/', Signup, name='signup'),
-	path('finduser/', include('finduser.urls')),
    	path('', authViews.LoginView.as_view(template_name='login.html'), name='login'),
    	path('logout/', authViews.LogoutView.as_view(), {'next_page' : 'index'}, name='logout'),
    	path('changepassword/', PasswordChange, name='change_password'),
